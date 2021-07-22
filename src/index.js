4f825a84b7a08c7cf1c0067d4df53a0a93054389
Ahsanch01 <71697051+Ahsanch01@users.noreply.github.com>
2021-07-22 10:57:35 +0500

    second

--- src/index.js
+++ src/index.js
@@ -4,38 +4,38 @@ import SeasonDisplay from './seasonDisplay';
 import Spiner from './Spiner';
 import App1 from './components/App';
 class App extends React.Component{
-    // state={lat:null,errorMessage:''}
-    // componentDidMount(){
-    //     window.navigator.geolocation.getCurrentPosition(
-    //         (position)=>{this.setState({lat:position.coords.latitude})},
-    //         (err)=>{this.setState({errorMessage:err.message})}
-    //     )
-    // }
-
-    // renderContent(){
-    //     if (this.state.errorMessage && !this.state.lat){
-    //         return <div> Error:{this.state.errorMessage}</div>
-    //     }
-    //     if(!this.state.errorMessage && this.state.lat){
-    //         return<div><SeasonDisplay lat={this.state.lat} /></div>
-    //     }
-       
-    //     return <Spiner message='Allow to access location for smooth working'/>
-    // }
-    // render(){ 
-    //     return(
-    //         <div className='border red'>
-    //             {this.renderContent()}
-    //         </div>
-    //     )
-       
-    // }
-
-    render(){
-        return(
-            <div><App1 /></div>
+    state={lat:null,errorMessage:''}
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{this.setState({lat:position.coords.latitude})},
+            (err)=>{this.setState({errorMessage:err.message})}
         )
     }
+
+    renderContent(){
+        if (this.state.errorMessage && !this.state.lat){
+            return <div> Error:{this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return<div><SeasonDisplay lat={this.state.lat} /></div>
+        }
+       
+        return <Spiner message='Allow to access location for smooth working'/>
+    }
+    render(){ 
+        return(
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        )
+       
+    }
+
+    // render(){
+    //     return(
+    //         <div><App1 /></div>
+    //     )
+    // }
 }
 reactDOM.render(<App />,
     document.querySelector('#root'));
